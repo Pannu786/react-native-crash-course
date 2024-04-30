0c6cd2e142c213b6068690e63fe3aabf39903042
Pannu786 <thepannu786@gmail.com>
2024-04-30 17:21:28 +0530

    done the basic settings and task.. will install the nativewind soon

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Link } from 'expo-router';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Aora!! Pannu Index Page </Text>
+      <StatusBar style='auto' />
+      <Link href={'/profile'} style={{ color: 'blue' }}>
+        Click for Link
+      </Link>
     </View>
   );
 }
---
+++ app/profile.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 30, color: 'red' }}>Profile Mf</Text>
+    </View>
+  );
+};
+
+export default Profile;
+
+const styles = StyleSheet.create({});
