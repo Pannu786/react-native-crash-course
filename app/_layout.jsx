bef49cd0987220656ba137a6c52d23dfd1d3c467
Pannu786 <thepannu786@gmail.com>
2024-05-04 23:09:02 +0530

    Importing fonts in layout cmp

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { Slot, Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
 
 const RootLayout = () => {
+  
   return (
     <Stack>
       <Stack.Screen name='index' options={{ headerShown: false }} />
