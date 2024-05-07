f9a7bf8fc76b2057c7b12889ce6d6e30e590cc26
Pannu786 <thepannu786@gmail.com>
2024-05-07 22:09:31 +0530

    #

--- app/index.jsx
+++ app/index.jsx
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native';
 import { Link } from 'expo-router';
 export default function App() {
   return (
-    <View className='flex-1 items-center justify-center bg-black'>
-      <Text className='text-pink-500 tex-4xl'>Aora!! Pannu Index Page </Text>
+    <View className='flex-1 items-center justify-center bg-white'>
+      <Text className='font-pblack tex-4xl'>Aora!! Pannu Index Page </Text>
       <StatusBar style='auto' />
       <Link href={'/profile'} className='text-yellow-500 font-extrabold'>
         Click for Link
