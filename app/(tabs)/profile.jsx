d1a38ccc51fba47f4bd07f667e0032a1d1719888
Pannu786 <thepannu786@gmail.com>
2024-05-11 21:37:38 +0530

    set-up folder for different pages

---
+++ app/(tabs)/profile.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <View className='flex-1 items-center justify-center bg-orange-500'>
+      <Text className='text-purple-500 font-extrabold text-4xl '>
+        Profile Mf
+      </Text>
+    </View>
+  );
+};
+
+export default Profile;
